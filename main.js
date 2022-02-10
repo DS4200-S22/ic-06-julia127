3847423f72e056377dd9a9be790a84720e0444ff
julia127 <45829660+julia127@users.noreply.github.com>
2022-02-10 13:27:40 -0500

    ic5

--- main.js
+++ main.js
@@ -10,5 +10,30 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let myRand = Math.random()*100; // the number we want to show up in the random_num div
+
+    // put myRand into div
+    let button = document.getElementById("random_num");
+    button.innerHTML = myRand;
+
 }
+
+// when Button 2 is clicked the circle changes color.
+// If the circle is blue, clicking Button 2 should make it green. 
+// If the circle is green, clicking Button 2 should make it blue
+function button2Clicked() {
+
+    let button = document.getElementById("cir");
+
+    if (circle.style.fill == green) {
+        button.style.fill = blue;
+    } else {
+        button.style.fill = green;
+    }
+
+}
+
+// declare variables with cont and let
+// event handler and event listener 
+    // events -> what user does with web page
+    // listener -> looks for user's interaction 
